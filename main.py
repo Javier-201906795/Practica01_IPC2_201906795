8ee136e3c06b6e48d2244b929fa4c723ba37f1ca
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2025-02-19 11:36:39 -0600

    comentar lineas de cuante de ahorro

--- main.py
+++ main.py
@@ -5,23 +5,25 @@ if __name__ == "__main__":
     #Base de Datos
     DBCuentas = None
 
-    #Crear cuenta
-    cuenta1 = CuentaAhorro(1,'Juan Lopez', 1000, 0.1)
+    # #Crear cuenta
+    # cuenta1 = CuentaAhorro(1,'Juan Lopez', 1000, 0.1)
 
-    #Imprimir cuenta
-    print(cuenta1.mostarinformacion())
-    print(cuenta1.mostarsaldo())
+    # #Imprimir cuenta
+    # print(cuenta1.mostarinformacion())
+    # print(cuenta1.mostarsaldo())
 
-    #Depositar
-    cuenta1.depositar(100)
-    print(cuenta1.mostarsaldo())
+    # #Depositar
+    # cuenta1.depositar(100)
+    # print(cuenta1.mostarsaldo())
     
-    #Retirar
-    cuenta1.retirar(50)
-    print(cuenta1.mostarsaldo())
+    # #Retirar
+    # cuenta1.retirar(50)
+    # print(cuenta1.mostarsaldo())
     
-    #Calcular Interes
-    cuenta1.calcularinteres()
-    print(cuenta1.mostarsaldo())
+    # #Calcular Interes
+    # cuenta1.calcularinteres()
+    # print(cuenta1.mostarsaldo())
+
+
 
  
